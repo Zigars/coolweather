f46e122bab4fb7473bd12b783f643e3c3649ab3c
ZHT <ZHT@gmail.com>
2017-10-26 21:40:39 +0800

    完成显示天气信息

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by Administrator on 2017/10/26.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
